e22e6fc50fbdc8fa472f5c5e2452bf498d4017f7
dengchaogithub <dengchao@mininglamp.com>
2020-09-01 11:16:35 +0800

    Initial commit

---
+++ src/abc.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.util.List;
+
+public class abc {
+    public static void main(String[] args) {
+        System.out.println();
+        String he = "he";
+        if (he == null) {
+
+        }
+        if (he != null) {
+
+        }
+        if (he != null) {
+            
+        }
+        List<String> lists = new Arrylist<>();
+        for (int i = 0; i < lists.size(); i++) {
+
+        }
+        for (String list : lists) {
+
+        }
+        object pi=3.1415;
+        if (pi>0) {
+
+        }
+        return pi;
+
+
+        try {
+            File file =new File()
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
